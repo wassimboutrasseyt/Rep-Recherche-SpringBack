8f60823f3d8a007169d5ffa88a107cb66149b74e
wassim boutrasseyt <boutrasseytwassim@gmail.com>
2024-12-02 17:30:55 +0100

    initialise the project

---
+++ src/main/java/org/sid/appbackser/AppBackSerApplication.java
@@ -0,0 +1,13 @@
+package org.sid.appbackser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBackSerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBackSerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/appbackser/AppBackSerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.appbackser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBackSerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
